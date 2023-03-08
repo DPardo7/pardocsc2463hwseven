0c9e4c86a9b2216c74c927ccc4b62f779234b4f1
DPardo7 <99456649+DPardo7@users.noreply.github.com>
2023-03-08 00:00:16 -0600

    Update sketch.js

--- js/sketch.js
+++ js/sketch.js
@@ -5,23 +5,23 @@ let osc = new Tone.AMOscillator(pitch, 'sine', 'sine').start();
 let gain = new Tone.Gain().toDestination();
 let pan = new Tone.Panner().connect(gain);
 let ampEnv = new Tone.AmplitudeEnvelope({
-  attack: 0.1,
+  attack: 0.7,
   decay: 0.4,
   sustain: 1.0,
   release: 0.4
 }).connect(pan);
 osc.connect(ampEnv);
 
-let noise = new Tone.Noise('pink').start();
-let noiseEnv = new Tone.AmplitudeEnvelope({
+let ac = new Tone.Noise('pink').start();
+let acEnv = new Tone.AmplitudeEnvelope({
   attack: 0.03,
   decay: 0.3,
   sustain: 1.0,
-  release: 0.8
+  release: 0.1
 }).connect(gain);
 
 let acFilter = new Tone.Filter(1000, "lowpass").connect(acEnv);
-noise.connect(acFilter)
+ac.connect(acFilter)
 
 function setup() {
   createCanvas(2000, 300)
@@ -36,21 +36,16 @@ function draw() {
   }
 
   text('--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------', 0, 210);
-  text('Warning...', 610, 225);
-  text('press down here for alarm everyone!', 540, 245);
+  text('After a long day in the heat...', 610, 225);
+  text('press down here for a short burst of the AC in your car!', 540, 245);
 
 }
 
 function mousePressed() {
   console.log('pressed');
 
-  ampEnv.triggerAttackRelease('8n');
-  osc.frequency.setValueAtTime(pitch + 200, '+1');
-  ampEnv.triggerAttackRelease('4n');
-  drum.triggerAttackRelease(ac);
-
   if (mouseY > 200) {
-    Env.triggerAttackRelease(0.5);
+    acEnv.triggerAttackRelease(0.5);
   }
 
 }
