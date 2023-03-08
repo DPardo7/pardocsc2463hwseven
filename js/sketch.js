6a8555d0aac8de278d1943fae95ee1c1a02e2586
DPardo7 <99456649+DPardo7@users.noreply.github.com>
2023-03-07 23:54:37 -0600

    Update sketch.js

--- js/sketch.js
+++ js/sketch.js
@@ -5,23 +5,23 @@ let osc = new Tone.AMOscillator(pitch, 'sine', 'sine').start();
 let gain = new Tone.Gain().toDestination();
 let pan = new Tone.Panner().connect(gain);
 let ampEnv = new Tone.AmplitudeEnvelope({
-  attack: 0.7,
+  attack: 0.1,
   decay: 0.4,
   sustain: 1.0,
   release: 0.4
 }).connect(pan);
 osc.connect(ampEnv);
 
-let ac = new Tone.Noise('pink').start();
-let acEnv = new Tone.AmplitudeEnvelope({
+let noise = new Tone.Noise('pink').start();
+let noiseEnv = new Tone.AmplitudeEnvelope({
   attack: 0.03,
   decay: 0.3,
   sustain: 1.0,
-  release: 0.9
+  release: 0.8
 }).connect(gain);
 
 let acFilter = new Tone.Filter(1000, "lowpass").connect(acEnv);
-ac.connect(acFilter)
+noise.connect(acFilter)
 
 function setup() {
   createCanvas(2000, 300)
@@ -44,8 +44,13 @@ function draw() {
 function mousePressed() {
   console.log('pressed');
 
+  ampEnv.triggerAttackRelease('8n');
+  osc.frequency.setValueAtTime(pitch + 200, '+1');
+  ampEnv.triggerAttackRelease('4n');
+  drum.triggerAttackRelease(ac);
+
   if (mouseY > 200) {
-    acEnv.triggerAttackRelease(0.5);
+    Env.triggerAttackRelease(0.5);
   }
 
 }
